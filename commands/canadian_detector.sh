dc47a959bb42b0e1e52a96ea2f477428ddff8744
Michael Holzinger <mike.holzinger@gmail.com>
2016-11-18 16:33:33 -0800

    le formatting

--- commands/canadian_detector.sh
+++ commands/canadian_detector.sh
@@ -1,10 +1,11 @@
-export SL_USERNAME=<YOUR_ACCOUNTID_HERE>
-export SL_API_KEY=<YOUR_KEY_HERE>
+#export SL_USERNAME=<YOUR_ACCOUNTID_HERE>
+#export SL_API_KEY=<YOUR_KEY_HERE>
 
-slcli ticket list \
-  awk '{print $1}' \
+slcli ticket list |\
+  awk '{print $1}' |\
   grep '[0-9]'|\
   while read ticketid
-    do 
+    do
       slcli ticket detail "$ticketid"|grep -E 'sorry|apologize|fault|soorry'
     done
+
